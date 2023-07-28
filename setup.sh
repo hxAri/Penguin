1c0ad8236f3ed7ad71094ade86d547119773913f
hxAri <hxari@proton.me>
2023-07-28 23:02:08 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+#
+# @author Ari Setiawan
+# @create 11.07-2023
+# @github https://github.com/hxAri/Penguin
+#
+# Penguin Copyright (c) 2023 - Ari Setiawan <hxari@proton.me>
+# Penguin Licence under GNU General Public Licence v3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
 installs=()
 requires=(
 	"curl"
